f7b3bd97778e11ff95daa9ef669503b12836d6a9
Christoph Koerner <christoph.koerner@gesim.ch>
2015-12-07 19:41:54 +0100

    Fixed index bug

--- python/chris/utils.py
+++ python/chris/utils.py
@@ -3,17 +3,16 @@ import time, datetime
 import numpy as np
 import skutils
 
-@skutils.score(greater_is_better=False)
 def rmspe(y, y_pred):
     """Root Mean Square Percentage Error
-    Details about this function can be found on kaggle 
+    Details about this function can be found on kaggle
     https://www.kaggle.com/c/rossmann-store-sales/details/evaluation"""
     # Convert the values to numpy arrays
     y, y_pred = np.array(y), np.array(y_pred)
     # Create a weight vector, and initialize with zeros
     w = np.zeros(y.shape, dtype=float)
     # Create a binary maks containing indecies
-    # of all non-zero values 
+    # of all non-zero values
     idx = y != 0
     # Add weights for all non-zero values
     w[idx] = 1.0 / (y[idx] ** 2)
@@ -22,3 +21,7 @@ def rmspe(y, y_pred):
 
 def timestamp():
     return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+@skutils.score(greater_is_better=False)
+def rmspe_log1p(y, y_pred):
+    return rmspe(np.expm1(y), np.expm1(y_pred))
